d595c17de9f7eb96fe9413f728448ece30399fd5
Natanael Xavier <natanaelxavier@icloud.com>
2020-12-16 21:10:42 -0300

    Backgroud e Separação. Tela Inicial e tela de Mensagens.

--- Chat Socket.io/server.js
+++ Chat Socket.io/server.js
@@ -1,19 +1,26 @@
 const { Socket } = require('dgram');
 const express = require('express');
+const bodyParser = require('body-parser')
 const path = require('path');
 
 const app = express();
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname,'public')));
-app.set('views',path.join(__dirname,'public'));
+app.set('views',path.join(__dirname,'Paginas'));
 app.engine('html',require('ejs').renderFile);
 app.set('view engine','html');
 
-app.use('/',(req,res)=>{
+app.get('/',(req,res)=>{
     res.render('index.html')
+})
+
+app.post('/acessar',(req,res)=>{
+    let Usuario = req.body.usuario;
+    res.render('chat.html',{Usuario: Usuario})
 })
 
 let messages = []
